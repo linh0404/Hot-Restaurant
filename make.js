b51a2226ee5aa6ced76cd7978a8ab76eac7149fa
Linh Nguyen <linh0404@hotmail.com>
2019-09-07 13:08:54 +1000

    added jquery to take in form details

---
+++ make.js
@@ -0,0 +1,17 @@
+//Capture Button Click
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    //Grabbed values from text boxes
+    var data = {
+    name: $("#name-input").val().trim(),
+    phoneNo: $("#phone-input").val().trim(),
+    email: $("#email-input").val().trim(),
+    uniqueId: $("#uniqueid-input").val().trim(),
+    }
+
+    $.post( "api/reserve", data );
+    
+});
+
+
