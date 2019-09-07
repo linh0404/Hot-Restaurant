50aab7942bebc604133a55a957c84126fde04c84
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-07 12:04:34 +1000

    html get links done

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,9 +2,22 @@ var path = require("path");
 
 function htmlroutes(app){
 
-    app.get("/",function(req,res){
+    // app.get("/",function(req,res){
+    //     res.sendFile(path.join(__dirname,"../index.html"));
+    // });
+
+    app.get("/view",function(req,res){
+        res.sendFile(path.join(__dirname,"../view.html"));
+    });
+
+    app.get("/reserve",function(req,res){
+        res.sendFile(path.join(__dirname,"../make.html"));
+    });
+
+    app.get("*",function(req,res){
         res.sendFile(path.join(__dirname,"../index.html"));
     });
 }
+
 
 module.exports=htmlroutes
