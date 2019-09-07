6fdbbd6bbd4ccd071f092217597e49baaedd305a
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-07 11:13:29 +1000

    add files

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routes/api")(api)
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
