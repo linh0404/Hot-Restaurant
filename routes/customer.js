110ded666a81c38801dc1aef48e3413e81ae6d33
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-07 11:09:25 +1000

    gett the router working

---
+++ routes/customer.js
@@ -0,0 +1,20 @@
+
+
+var customers = [
+    {
+customer_Name:"John",
+customer_Phone:0404040404,
+customer_Email:"test@gmail.com",
+customer_UniqueID:"12345"
+    },
+
+]
+
+
+module.exports = {
+    customer_Name: customer_Name,
+    customer_Phone: customer_Phone,
+    customer_Email:customer_Email,
+    customer_UniqueID:customer_UniqueID
+  };
+  
--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var api = require("./routes/api");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
+app.use("/api",api);
 var PORT = 3000;    
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
