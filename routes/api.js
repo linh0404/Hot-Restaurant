05fa3c70f8550fc88234ffa89f0abc1d6faa7252
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-07 11:30:54 +1000

    added html.js

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+// var express = require("express");
+// var router = express.Router();
 
 router.get("/", function (req,res) {
     res.sendfile(path.join(__dirname,"index.html"));
--- server.js
+++ server.js
@@ -2,19 +2,21 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-var api = require("./routes/api");
+// var api = require("./routes/api");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-app.use("/api",api);
+// app.use("/api",api);
 var PORT = 3000;    
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/api")(api)
+
+require("./routes/htmlroutes")(app)
+
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
